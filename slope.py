e6dc6334d838c0c85849cca74dcb7aa868f636f4
jnep62 <ipapaspi@env.duth.gr>
2024-06-18 00:42:20 +0300

    Update slope.py

--- slope.py
+++ slope.py
@@ -115,8 +115,8 @@ with col2:
 
     X = array([ε, θ, L, Qs_a, Qs_b, Qs_c, Qs_d]).reshape(1, 7)
     # load scaling parameters
-    x_scaler = joblib.load('x_scaler.joblib')
-    y_scaler = joblib.load('y_scaler.joblib')
+    x_scaler = joblib.load("./x_scaler.joblib")
+    y_scaler = joblib.load("./y_scaler.joblib")
     # scale the input data (do not use .fit_transform here)
     X = x_scaler.transform(X)
     # scaling done, convert to tensor for the model
