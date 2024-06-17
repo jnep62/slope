3264fb97b7597f5dc6fccb3e120dcba3262ae248
jnep62 <ipapaspi@env.duth.gr>
2024-06-18 01:03:23 +0300

    Update slope.py

--- slope.py
+++ slope.py
@@ -115,8 +115,7 @@ with col2:
 
     X = array([ε, θ, L, Qs_a, Qs_b, Qs_c, Qs_d]).reshape(1, 7)
     # load scaling parameters
-    x_scaler = open("x_scaler.joblib", "rb")
-    joblib.load(x_scaler)
+    x_scaler = joblib.load('x_scaler.joblib', 'rb')
     y_scaler = joblib.load('y_scaler.joblib')
     # scale the input data (do not use .fit_transform here)
     X = x_scaler.transform(X)
